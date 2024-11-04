8c07bef0c308817c1183c329a366a6f4d8da13b7
Michał Kapuściński <alert251c@gmail.com>
2024-11-04 19:23:00 +0100

    alu fixed

--- Projektowanie_Lab/ALU.v
+++ Projektowanie_Lab/ALU.v
@@ -4,17 +4,21 @@ module ALU(
     input clk, input alu_ce, input cy_ce, input [(`OPCODE_WIDTH - 1):0] opcode, input [7:0] register,
     output reg [7:0] acc);
 
+    initial begin
+        acc = 8'b0;
+    end
+
     always @(posedge clk) begin
         if (alu_ce) begin
             case (opcode)
-                `ADD: acc <= acc + register;
-                `SUB: acc <= acc - register;
-                `LD: acc <= register;
-                `AND: acc <= acc & register;
-                `OR: acc <= acc | register;
-                `XOR: acc <= acc ^ register;
-                `NOT: acc <= ~register;
-                default: acc <= acc;
+                `ADD: acc = acc + register;
+                `SUB: acc = acc - register;
+                `LD: acc = register;
+                `AND: acc = acc & register;
+                `OR: acc = acc | register;
+                `XOR: acc = acc ^ register;
+                `NOT: acc = ~register;
+                default: acc = acc;
             endcase
         end
     end
--- Projektowanie_Lab/RF.v
+++ Projektowanie_Lab/RF.v
@@ -7,20 +7,20 @@ module RF(
     input [7:0] acc,
     input [7:0] debug_reg, 
     input [(`REG_ADDR_WIDTH - 1):0] addr, 
-    output reg [7:0] data);
+    output [7:0] data);
 
     reg [7:0] registers [3:0];
 
-    always @(posedge clk) begin
-        if (ld_ce) begin
-            if (addr == `R3) begin
-                data <= debug_reg;
-            end
-            else begin
-                data <= registers[addr];
-            end
-        end
+    initial begin
+        registers[0] = 8'b0;
+        registers[1] = 8'b0;
+        registers[2] = 8'b0;
+        registers[3] = 8'b0;
+    end
 
+    assign data = (addr == `R3) ? debug_reg : registers[addr];
+
+    always @(posedge clk) begin
         if (st_ce) begin
             registers[addr] <= acc;
         end
